f95140fc1a17aef9f7cc644d6b6de54f09423408
SammyJoe-Rammouz <99485892+SammyJoe-Rammouz@users.noreply.github.com>
2022-03-18 18:58:28 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,53 @@
+package Lab6;
+
+public class Calculator {
+	 private int num1;
+	    private int num2;
+	    private String operator;
+
+	    public Calculator() {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.print("Input the first number: ");
+	        num1 = Integer.parseInt(scan.nextLine());
+
+	        System.out.print("Input the operator (+,-,x,/): ");
+	        operator = scan.nextLine();
+
+	        System.out.print("Input the second number: ");
+	        num2= Integer.parseInt(scan.nextLine());
+
+	        switch (operator){
+	            case "+":
+	                System.out.println(num1 + num2);
+	                break;
+	            case "-":
+	                System.out.println(num1 - num2);
+	                break;
+	            case "x":
+	                System.out.println(num1 * num2);
+	                break;
+	            case "/":
+	                System.out.println(num1/num2);
+	                break;
+	            default:
+	                break;
+	        }
+	    }
+	    public String add(int a, int b) {
+	        int sum=a+b;
+	        return a+"+"+b+"="+sum;
+	    }
+	    public String subtract(int a, int b) {
+	        int diff = a-b;
+	        return a + "-"+ b + "="+ diff;
+	    }
+	    public String multiply(int a, int b) {
+	        int prod = a*b;
+	        return a + "+" + b + "=" + prod;
+	    }
+	    public String divide(int a, int b) {
+	        int quot = a/b;
+	        return a + "/" +b+"="+quot;
+	    }
+
+}
